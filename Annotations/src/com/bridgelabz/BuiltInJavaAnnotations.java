9afc009ad1678e8d5935ed2dfc8d53969ab5ee53
saurav-manchanda <saurav.manchanda2009@gmail.com>
2018-06-22 15:18:02 +0530

    Annotations examples

---
+++ Annotations/src/com/bridgelabz/BuiltInJavaAnnotations.java
@@ -0,0 +1,44 @@
+/********************************************************************************* *
+ * Purpose: To show use of built in annotations in java using basic program
+ * 
+ * @author Saurav Manchanda
+ * @version 1.0
+ * @since 22/06/2018
+ *********************************************************************************/
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * @author Saurav:
+ * Class Book
+ */
+class Book{
+	public void bookName() {
+		System.out.println("Hindi Vyakaran");
+	}
+}
+/**
+ * @author Saurav:
+ * Class to show the use of Built in Java Annotations
+ */
+public class BuiltInJavaAnnotations extends Book{
+	@Override
+	public void bookName()// Overrided method thats why @overriding is used
+	{
+		System.out.println("English grammar");
+	}
+	@Deprecated
+	public void inputString()//We should not use this method because it is deprecated
+	{
+		System.out.println("Please enter a String");
+		@SuppressWarnings("resource")//scan should be closed..thats why to supress the warning we use this
+		Scanner scan=new Scanner(System.in);
+		String string=scan.next();
+		System.out.println("The Entered Strings is "+ string);
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+}
---
+++ Annotations/src/com/bridgelabz/MetaAnnotationTest.java
@@ -0,0 +1,55 @@
+/********************************************************************************* *
+ * Purpose: To make custom Annotation and use built in Meta Annotations within the program
+ * 
+ * @author Saurav Manchanda
+ * @version 1.0
+ * @since 22/06/2018
+ *********************************************************************************/
+package com.bridgelabz;
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@Inherited
+@Documented
+@interface MyAnnotation {
+	int value();
+
+	String value2() default "Saurav";
+}
+
+/**
+ * @author Saurav:
+ * Class Hello having Annotation usage
+ */
+class Hello {
+	@MyAnnotation(value = 10)
+	public void sayHello() {
+		System.out.println("Hello Annotation");
+	}
+}
+
+/**
+ * @author Saurav:
+ * Class to show the use of Meta Annotations in java
+ */
+public class MetaAnnotationTest extends Hello {
+	public static void main(String[] args) throws NoSuchMethodException, SecurityException {
+		Hello hello=new Hello();
+		Method m1=hello.getClass().getMethod("sayHello");
+		MyAnnotation myAnnotation= m1.getAnnotation(MyAnnotation.class);//for using the annotation in Hello class
+		System.out.println("tha value is:"+myAnnotation.value()+ " and name is: "+myAnnotation.value2());
+		MetaAnnotationTest meta=new MetaAnnotationTest();
+		Method m2=meta.getClass().getMethod("sayHello");
+		MyAnnotation myAnnotation1=m2.getAnnotation(MyAnnotation.class);
+		System.out.println("tha value is:"+myAnnotation.value()+ " and name is: "+myAnnotation.value2());
+		
+	}
+
+}
